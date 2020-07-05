796cb40fbb98e52fe3ed7a1c19f13a7ea4b1db3f
Prachi <darshana.prachi@jungleworks.com>
2020-07-05 20:49:25 +0530

    Updates

---
+++ precache-manifest.05b7d8cc8256da0ee22168b6191e79df.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50b08434c15b7aee8b15ac60fbcef83a",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "8737192970ed8db58606",
+    "url": "/weather-app/static/css/main.59b1dd67.chunk.css"
+  },
+  {
+    "revision": "403e339d8adaf6d88c63",
+    "url": "/weather-app/static/js/2.85076cc0.chunk.js"
+  },
+  {
+    "revision": "b4cabae32341920cfb4131dbdebda2c2",
+    "url": "/weather-app/static/js/2.85076cc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8737192970ed8db58606",
+    "url": "/weather-app/static/js/main.684ddf3e.chunk.js"
+  },
+  {
+    "revision": "d0e2e579b5155f255804",
+    "url": "/weather-app/static/js/runtime-main.e6f382d7.js"
+  },
+  {
+    "revision": "c23534acbeddbaadfd0ab2d2bbfdfc84",
+    "url": "/weather-app/static/media/Poppins-Bold.c23534ac.ttf"
+  },
+  {
+    "revision": "41e8dead03fb979ecc23b8dfb0fef627",
+    "url": "/weather-app/static/media/Poppins-Regular.41e8dead.ttf"
+  },
+  {
+    "revision": "342ba3d8ac29ac8c38d7cef8efbf2dc9",
+    "url": "/weather-app/static/media/Poppins-SemiBold.342ba3d8.ttf"
+  },
+  {
+    "revision": "e9ac59a8a5fddc8c506aaa5d9924e813",
+    "url": "/weather-app/static/media/pin.e9ac59a8.svg"
+  },
+  {
+    "revision": "744d54d4d1b3e69dd25c6998b5c6c918",
+    "url": "/weather-app/static/media/search.744d54d4.svg"
+  }
+]);
